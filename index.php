aca42d50d377af1ca6d9e581d422daef16076dfd
hoheckell <hoheckell.info@gmail.com>
2017-05-06 19:29:30 -0300

    FIRST

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ZIP FILE AND DOWNLOAD</title>
+</head>
+<body>
+<h1>ZIP FILE AND DOWNLOAD</h1>
+<form name="file" action="" method="post" enctype="multipart/form-data">
+    <input type="file" name="file[]" multiple>
+    <button type="submit">Enviar</button>
+</form>
+
+<?php
+/**
+ *
+ * @author Hoheckell <hoheckell.info@gmail.com>
+ *
+ * As referẽncias abaixo em desenvolvimento sao como se o website estivesse em localhost/zipfile
+ *
+ */
+if (!empty($_FILES['file'])) {
+    try {
+
+        for ($i = 0; $i < count($_FILES["file"]["tmp_name"]); $i++) {
+
+            $tmp_name = $_FILES["file"]["tmp_name"][$i];
+            if (!$tmp_name) die('TMP NAME ERROR!');
+
+            $name = basename($_FILES["file"]["name"][$i]);
+            if (move_uploaded_file($tmp_name, "./" . $name)) {
+                echo "Enviando arquivo '" . $name . "'.<br/>\n";
+                $zip = new ZipArchive;
+                $filename = "./ZFAD_" . date("d_m_Y") . ".zip";
+                $c=true;
+                $t=1;
+                while($c){
+                    if(file_exists($filename)){
+                        $filename = "/ZFAD_" . date("d_m_Y") . "_".$c.".zip";
+                        $c=true;
+                        $t++;
+                    }else{
+                        $c=false;
+                    }
+                }
+
+                if ($zip->open($filename, ZipArchive::CREATE) !== TRUE) {
+                    exit("cannot open <$filename>\n");
+                }
+
+                $zip->addFile($_FILES['file']["name"][$i], $name);
+                echo "Arquivo " . $zip->numFiles . " ";
+                echo " " . ($zip->status === 0) ? "Enviado" : " Falhou";
+                echo "<br/>\n";
+                $zip->close();
+                unlink($_FILES['file']["name"][$i]);
+
+            } else {
+                die('CANT UPLOAD!');
+
+            }
+        }
+
+        echo '<a href="' . $filename . '">Baixar arquivo</a><br>';
+        echo '<a href="/zipfile">Compactar novo arquivo</a>';
+
+        if ($handle = opendir('.')) {
+            $data = date('d_m_Y');
+            $d = explode("_", $data);
+            while (false !== ($arquivo = readdir($handle))) {
+                if ($arquivo != "." && $arquivo != ".." && strtolower(substr($arquivo, strrpos($arquivo, '.') + 1)) == 'zip') {
+                    $arqname = explode('_', $arquivo);
+                    if ($arqname[1] < $d[0]) {
+                        unlink($arquivo);
+                    }
+                }
+            }
+            closedir($handle);
+        }
+
+    } catch (Exception $e) {
+        echo $e->getMessage;
+    }
+}
+?>
+</body>
+</html>
